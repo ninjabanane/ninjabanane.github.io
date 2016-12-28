9d7f02094bef44da07f1b1af3ebf4613d8e69875
ninjabanane <ninjabanane@live.de>
2016-12-28 02:31:46 +0100

    ss

--- scripts.js
+++ scripts.js
@@ -1,23 +1,3 @@
-function creatediv(){
-  var myStringArray = [
-  					   '<iframe src="news/1.html" width=100% frameborder=0 scrolling="no"></iframe>',
-  					   '<iframe src="news/2.html" width=100% frameborder=0 scrolling="no"></iframe>',
-  					   '<iframe src="news/1.html" width=100% frameborder=0 scrolling="no"></iframe>'
-  					  ];
-
-  var arrayLength = myStringArray.length;
-
-
-  for (var i = arrayLength-1; i >=0; i--) {	
-  	div = document.createElement("div");
-    div.setAttribute('class', 'applebox newsbox');
-    div.innerHTML = myStringArray[i];
-
-    document.getElementById("news").appendChild(div);
-  };
-}
-    
- 
 function news(){
 	
 	document.getElementById("maincontainer").innerHTML = document.getElementById("news");
